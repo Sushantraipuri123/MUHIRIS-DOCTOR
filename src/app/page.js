85b015dc0ea502109137aa21dfe7900641bb2392
Sushant Raipuri <cashu853@gmail.com>
2024-09-18 17:21:35 +0530

    learning nextjs

---
+++ src/app/page.js
@@ -0,0 +1,12 @@
+import  "bootstrap/dist/css/bootstrap.min.css"
+
+export default function Home() {
+  return (
+    <>
+   <h1 className="text-center bg-info py-3">Testing bootstrap</h1>
+   <div className="container mt-3">
+    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Temporibus ex id cumque veniam magnam dignissimos beatae consequuntur nostrum suscipit aspernatur eum, vitae aliquid dolore nam vero, exercitationem illum assumenda tempora?
+   </div>
+    </>
+  );
+}
