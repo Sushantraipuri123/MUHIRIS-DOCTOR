5c50f67ca09bbbf171cc300b249f50978e7123fd
Sushant Raipuri <cashu853@gmail.com>
2024-10-04 11:49:31 +0530

    test comit

--- src/app/page.js
+++ src/app/page.js
@@ -4,9 +4,7 @@ export default function Home() {
   return (
     <>
    <h1 className="text-center bg-info py-3">Testing bootstrap</h1>
-   <div className="container mt-3">
-    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Temporibus ex id cumque veniam magnam dignissimos beatae consequuntur nostrum suscipit aspernatur eum, vitae aliquid dolore nam vero, exercitationem illum assumenda tempora?
-   </div>
+  
     </>
   );
 }
