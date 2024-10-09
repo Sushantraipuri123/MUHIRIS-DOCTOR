729720b02488ee91b20270f5b42f70754b4c21be
Sushant Raipuri <cashu853@gmail.com>
2024-10-09 12:14:11 +0530

    added speciality cards

--- src/components/home/Specialty.jsx
+++ src/components/home/Specialty.jsx
@@ -1,19 +1,169 @@
-import React from 'react'
+"use client"
+import React from "react";
 
 function Specialty() {
   return (
-    <section className='container-fluid bg-theme py-5'>
-        <div className="container border">
-            <h2 className='plus fw-bold text-center theme-text-color display-5'>Our Specialty</h2>
-           <div className="d-flex justify-content-center">
-           <p className=" f-16 text-center inter text-black mt-2" style={{width:"347px"}}>
-           We provide the world class services with the | best medical team!
-            </p>
-           </div>
+    <section className="container-fluid bg-light-theme py-5">
+      <div className="container ">
+        <h2 className="plus fw-bold text-center theme-text-color display-5">
+          Our Specialty
+        </h2>
+        <div className="d-flex justify-content-center">
+          <p
+            className=" f-16 text-center inter text-black mt-2"
+            style={{ width: "347px" }}
+          >
+            We provide the world class services with the | best medical team!
+          </p>
         </div>
-       
+        <div className="mt-5 pt-5">
+          <div className="row justify-content-center gap-5">
+            <div className="col-lg-3 speciality-col">
+              <div>
+                <div className="scpeciality-icon-box d-flex align-items-center justify-content-center">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="50"
+                    height="50"
+                    viewBox="0 0 50 50"
+                    fill="none"
+                  >
+                    <path
+                      d="M26.7683 16.9131L31.0623 12.619M17.4646 17.6287L31.778 31.9421M8.87651 40.5302C4.92397 36.5777 4.92397 30.1693 8.87651 26.2168L26.0526 9.0407C30.0051 5.08816 36.4135 5.08816 40.366 9.0407C44.3185 12.9932 44.3185 19.4016 40.366 23.3541L23.1899 40.5302C19.2374 44.4827 12.8291 44.4827 8.87651 40.5302Z"
+                      stroke="#0086FF"
+                      stroke-width="2.42907"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                    />
+                  </svg>
+                </div>
+                <h3 className="mt-4 plus  fw-bold">Dentistry</h3>
+              </div>
+                <h5 className="inter speciality-subheading text-center f-19 fw-normal">Get consultation Form our Dentistry team</h5>
+            </div>
+            <div className="col-lg-3 speciality-col">
+              <div>
+                <div className="scpeciality-icon-box d-flex align-items-center justify-content-center">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="50"
+                    height="50"
+                    viewBox="0 0 50 50"
+                    fill="none"
+                  >
+                    <path
+                      d="M26.7683 16.9131L31.0623 12.619M17.4646 17.6287L31.778 31.9421M8.87651 40.5302C4.92397 36.5777 4.92397 30.1693 8.87651 26.2168L26.0526 9.0407C30.0051 5.08816 36.4135 5.08816 40.366 9.0407C44.3185 12.9932 44.3185 19.4016 40.366 23.3541L23.1899 40.5302C19.2374 44.4827 12.8291 44.4827 8.87651 40.5302Z"
+                      stroke="#0086FF"
+                      stroke-width="2.42907"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                    />
+                  </svg>
+                </div>
+                <h3 className="mt-4 plus  fw-bold">Dentistry</h3>
+              </div>
+                <h5 className="inter speciality-subheading text-center f-19 fw-normal">Get consultation Form our Dentistry team</h5>
+            </div>
+            <div className="col-lg-3 speciality-col">
+              <div>
+                <div className="scpeciality-icon-box d-flex align-items-center justify-content-center">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="50"
+                    height="50"
+                    viewBox="0 0 50 50"
+                    fill="none"
+                  >
+                    <path
+                      d="M26.7683 16.9131L31.0623 12.619M17.4646 17.6287L31.778 31.9421M8.87651 40.5302C4.92397 36.5777 4.92397 30.1693 8.87651 26.2168L26.0526 9.0407C30.0051 5.08816 36.4135 5.08816 40.366 9.0407C44.3185 12.9932 44.3185 19.4016 40.366 23.3541L23.1899 40.5302C19.2374 44.4827 12.8291 44.4827 8.87651 40.5302Z"
+                      stroke="#0086FF"
+                      stroke-width="2.42907"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                    />
+                  </svg>
+                </div>
+                <h3 className="mt-4 plus  fw-bold">Dentistry</h3>
+              </div>
+                <h5 className="inter speciality-subheading text-center f-19 fw-normal">Get consultation Form our Dentistry team</h5>
+            </div>
+          </div>
+
+          <div className="row mt-lg-5 justify-content-center gap-5">
+            <div className="col-lg-3 speciality-col">
+              <div>
+                <div className="scpeciality-icon-box d-flex align-items-center justify-content-center">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="50"
+                    height="50"
+                    viewBox="0 0 50 50"
+                    fill="none"
+                  >
+                    <path
+                      d="M26.7683 16.9131L31.0623 12.619M17.4646 17.6287L31.778 31.9421M8.87651 40.5302C4.92397 36.5777 4.92397 30.1693 8.87651 26.2168L26.0526 9.0407C30.0051 5.08816 36.4135 5.08816 40.366 9.0407C44.3185 12.9932 44.3185 19.4016 40.366 23.3541L23.1899 40.5302C19.2374 44.4827 12.8291 44.4827 8.87651 40.5302Z"
+                      stroke="#0086FF"
+                      stroke-width="2.42907"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                    />
+                  </svg>
+                </div>
+                <h3 className="mt-4 plus  fw-bold">Dentistry</h3>
+              </div>
+                <h5 className="inter speciality-subheading text-center f-19 fw-normal">Get consultation Form our Dentistry team</h5>
+            </div>
+            <div className="col-lg-3 speciality-col">
+              <div>
+                <div className="scpeciality-icon-box d-flex align-items-center justify-content-center">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="50"
+                    height="50"
+                    viewBox="0 0 50 50"
+                    fill="none"
+                  >
+                    <path
+                      d="M26.7683 16.9131L31.0623 12.619M17.4646 17.6287L31.778 31.9421M8.87651 40.5302C4.92397 36.5777 4.92397 30.1693 8.87651 26.2168L26.0526 9.0407C30.0051 5.08816 36.4135 5.08816 40.366 9.0407C44.3185 12.9932 44.3185 19.4016 40.366 23.3541L23.1899 40.5302C19.2374 44.4827 12.8291 44.4827 8.87651 40.5302Z"
+                      stroke="#0086FF"
+                      stroke-width="2.42907"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                    />
+                  </svg>
+                </div>
+                <h3 className="mt-4 plus  fw-bold">Dentistry</h3>
+              </div>
+                <h5 className="inter speciality-subheading text-center f-19 fw-normal">Get consultation Form our Dentistry team</h5>
+            </div>
+            <div className="col-lg-3 speciality-col">
+              <div>
+                <div className="scpeciality-icon-box d-flex align-items-center justify-content-center">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="50"
+                    height="50"
+                    viewBox="0 0 50 50"
+                    fill="none"
+                  >
+                    <path
+                      d="M26.7683 16.9131L31.0623 12.619M17.4646 17.6287L31.778 31.9421M8.87651 40.5302C4.92397 36.5777 4.92397 30.1693 8.87651 26.2168L26.0526 9.0407C30.0051 5.08816 36.4135 5.08816 40.366 9.0407C44.3185 12.9932 44.3185 19.4016 40.366 23.3541L23.1899 40.5302C19.2374 44.4827 12.8291 44.4827 8.87651 40.5302Z"
+                      stroke="#0086FF"
+                      stroke-width="2.42907"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                    />
+                  </svg>
+                </div>
+                <h3 className="mt-4 plus  fw-bold">Dentistry</h3>
+              </div>
+                <h5 className="inter speciality-subheading text-center f-19 fw-normal">Get consultation Form our Dentistry team</h5>
+            </div>
+          </div>
+        </div>
+      </div>
     </section>
-  )
+  );
 }
 
-export default Specialty
+export default Specialty;
