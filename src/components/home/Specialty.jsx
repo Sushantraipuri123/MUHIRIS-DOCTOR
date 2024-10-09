0de06356f6de638817c512d484ae01062081a959
Sushant Raipuri <cashu853@gmail.com>
2024-10-09 12:57:51 +0530

    created cards of speciality

--- src/components/home/Specialty.jsx
+++ src/components/home/Specialty.jsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import React from "react";
 
 function Specialty() {
@@ -39,11 +39,13 @@ function Specialty() {
                 </div>
                 <h3 className="mt-4 plus  fw-bold">Dentistry</h3>
               </div>
-                <h5 className="inter speciality-subheading text-center f-19 fw-normal">Get consultation Form our Dentistry team</h5>
+              <h5 className="inter speciality-subheading text-center f-19 fw-normal">
+                Get consultation Form our Dentistry team
+              </h5>
             </div>
             <div className="col-lg-3 speciality-col">
               <div>
-                <div className="scpeciality-icon-box d-flex align-items-center justify-content-center">
+                <div className="scpeciality-icon-box mx-auto d-flex align-items-center justify-content-center">
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
                     width="50"
@@ -52,7 +54,7 @@ function Specialty() {
                     fill="none"
                   >
                     <path
-                      d="M26.7683 16.9131L31.0623 12.619M17.4646 17.6287L31.778 31.9421M8.87651 40.5302C4.92397 36.5777 4.92397 30.1693 8.87651 26.2168L26.0526 9.0407C30.0051 5.08816 36.4135 5.08816 40.366 9.0407C44.3185 12.9932 44.3185 19.4016 40.366 23.3541L23.1899 40.5302C19.2374 44.4827 12.8291 44.4827 8.87651 40.5302Z"
+                      d="M21.7821 4.9165L33.3315 16.4659M44.8809 28.0153L33.3315 16.4659M37.6625 12.1349L33.3315 16.4659M33.3315 16.4659L26.1131 9.24753L11.6764 23.6843C8.48709 26.8736 8.48709 32.0444 11.6764 35.2337L13.1201 36.6774M33.3315 16.4659L40.5499 23.6843L26.1131 38.1211C22.9239 41.3103 17.753 41.3103 14.5637 38.1211L13.1201 36.6774M13.1201 36.6774L6.89245 42.9049M16.0074 28.0153L21.7821 33.79"
                       stroke="#0086FF"
                       stroke-width="2.42907"
                       stroke-linecap="round"
@@ -60,104 +62,225 @@ function Specialty() {
                     />
                   </svg>
                 </div>
-                <h3 className="mt-4 plus  fw-bold">Dentistry</h3>
+                <h3 className="mt-4 plus  fw-bold">General Diagnosis</h3>
               </div>
-                <h5 className="inter speciality-subheading text-center f-19 fw-normal">Get consultation Form our Dentistry team</h5>
+              <h5 className="inter speciality-subheading text-center f-19 fw-normal">
+                Get consultation form our  General Diagnosis team
+              </h5>
             </div>
             <div className="col-lg-3 speciality-col">
               <div>
-                <div className="scpeciality-icon-box d-flex align-items-center justify-content-center">
+                <div className="scpeciality-icon-box mx-auto d-flex align-items-center justify-content-center">
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
-                    width="50"
-                    height="50"
-                    viewBox="0 0 50 50"
+                    width="115"
+                    height="114"
+                    viewBox="0 0 115 114"
                     fill="none"
                   >
+                    <rect
+                      x="0.693726"
+                      y="0.484375"
+                      width="113.356"
+                      height="113.356"
+                      rx="25.91"
+                      fill="#F6F9FE"
+                    />
                     <path
-                      d="M26.7683 16.9131L31.0623 12.619M17.4646 17.6287L31.778 31.9421M8.87651 40.5302C4.92397 36.5777 4.92397 30.1693 8.87651 26.2168L26.0526 9.0407C30.0051 5.08816 36.4135 5.08816 40.366 9.0407C44.3185 12.9932 44.3185 19.4016 40.366 23.3541L23.1899 40.5302C19.2374 44.4827 12.8291 44.4827 8.87651 40.5302Z"
+                      d="M58.7975 69.6V43.2M58.7975 69.6C58.7975 64.2981 63.0956 60 68.3975 60M58.7975 69.6C58.7975 64.2981 54.4994 60 49.1975 60M58.7975 69.6C58.7975 72 59.1249 72.8886 59.9975 74.4C62.3171 78.4177 67.4545 79.7942 71.4721 77.4746C75.1562 75.3476 76.6196 70.8514 75.0487 67.0217C79.4648 66.159 82.7975 62.2687 82.7975 57.6C82.7975 52.2981 78.4994 48 73.1975 48L73.1975 43.2C73.1975 39.2236 69.974 36 65.9975 36C62.0211 36 58.7975 39.2236 58.7975 43.2M58.7975 69.6C58.7975 72 58.4701 72.8886 57.5975 74.4C55.2779 78.4177 50.1406 79.7942 46.1229 77.4746C42.4388 75.3476 40.9755 70.8514 42.5463 67.0217C38.1302 66.159 34.7976 62.2687 34.7976 57.6C34.7976 52.2981 39.0956 48 44.3976 48L44.3975 43.2C44.3975 39.2236 47.6211 36 51.5975 36C55.574 36 58.7975 39.2236 58.7975 43.2"
                       stroke="#0086FF"
-                      stroke-width="2.42907"
+                      stroke-width="2.5"
                       stroke-linecap="round"
                       stroke-linejoin="round"
                     />
                   </svg>
                 </div>
-                <h3 className="mt-4 plus  fw-bold">Dentistry</h3>
+                <h3 className="mt-4 plus  fw-bold">Neuro Surgery</h3>
               </div>
-                <h5 className="inter speciality-subheading text-center f-19 fw-normal">Get consultation Form our Dentistry team</h5>
+              <h5 className="inter speciality-subheading text-center f-19 fw-normal">
+                Get consultation form our Neuro Surgery team
+              </h5>
             </div>
           </div>
 
           <div className="row mt-lg-5 justify-content-center gap-5">
             <div className="col-lg-3 speciality-col">
               <div>
-                <div className="scpeciality-icon-box d-flex align-items-center justify-content-center">
+                <div className="scpeciality-icon-box mx-auto d-flex align-items-center justify-content-center">
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
-                    width="50"
-                    height="50"
-                    viewBox="0 0 50 50"
+                    width="114"
+                    height="114"
+                    viewBox="0 0 114 114"
                     fill="none"
                   >
+                    <rect
+                      x="0.434204"
+                      y="0.301086"
+                      width="113.356"
+                      height="113.356"
+                      rx="25.91"
+                      fill="#F6F9FE"
+                    />
                     <path
-                      d="M26.7683 16.9131L31.0623 12.619M17.4646 17.6287L31.778 31.9421M8.87651 40.5302C4.92397 36.5777 4.92397 30.1693 8.87651 26.2168L26.0526 9.0407C30.0051 5.08816 36.4135 5.08816 40.366 9.0407C44.3185 12.9932 44.3185 19.4016 40.366 23.3541L23.1899 40.5302C19.2374 44.4827 12.8291 44.4827 8.87651 40.5302Z"
+                      d="M78.5648 54.8127C79.2432 52.4783 79.6268 50.0715 79.6268 47.6293C79.6268 39.1342 73.8611 32.2474 66.7488 32.2474C62.3933 32.2474 58.5428 34.83 56.2122 38.783C53.8816 34.83 50.0311 32.2474 45.6756 32.2474C38.5632 32.2474 32.7975 39.1342 32.7975 47.6293C32.7975 63.0111 48.0171 76.9946 56.2122 80.2474C58.2367 79.4438 60.69 77.9853 63.2366 76.0126M55.0414 57.4177H60.8951L65.578 68.6045L70.261 51.8243L74.9439 63.0111H80.7975"
                       stroke="#0086FF"
-                      stroke-width="2.42907"
+                      stroke-width="2.5"
                       stroke-linecap="round"
                       stroke-linejoin="round"
                     />
                   </svg>
                 </div>
-                <h3 className="mt-4 plus  fw-bold">Dentistry</h3>
+                <h3 className="mt-4 plus  fw-bold">Cardiology</h3>
               </div>
-                <h5 className="inter speciality-subheading text-center f-19 fw-normal">Get consultation Form our Dentistry team</h5>
+              <h5 className="inter speciality-subheading text-center f-19 fw-normal">
+                Get consultation form our  Cardiology team
+              </h5>
             </div>
             <div className="col-lg-3 speciality-col">
               <div>
-                <div className="scpeciality-icon-box d-flex align-items-center justify-content-center">
+                <div className="scpeciality-icon-box mx-auto d-flex align-items-center justify-content-center">
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
-                    width="50"
-                    height="50"
-                    viewBox="0 0 50 50"
+                    width="114"
+                    height="114"
+                    viewBox="0 0 114 114"
                     fill="none"
                   >
+                    <rect
+                      x="0.0639877"
+                      y="0.301086"
+                      width="113.356"
+                      height="113.356"
+                      rx="25.91"
+                      fill="#F6F9FE"
+                    />
                     <path
-                      d="M26.7683 16.9131L31.0623 12.619M17.4646 17.6287L31.778 31.9421M8.87651 40.5302C4.92397 36.5777 4.92397 30.1693 8.87651 26.2168L26.0526 9.0407C30.0051 5.08816 36.4135 5.08816 40.366 9.0407C44.3185 12.9932 44.3185 19.4016 40.366 23.3541L23.1899 40.5302C19.2374 44.4827 12.8291 44.4827 8.87651 40.5302Z"
+                      d="M47.7975 65.7962H65.7975M56.7975 57.0157V74.5767M76.7856 49.5225C79.3745 52.4692 80.7975 56.2222 80.7975 60.1032V81.8938C80.7975 85.1267 78.1112 87.7474 74.7975 87.7474H38.7975C35.4838 87.7474 32.7975 85.1267 32.7975 81.8938V60.1032C32.7975 56.2222 34.2205 52.4692 36.8094 49.5225C39.9754 45.9189 44.5973 43.845 49.4622 43.845H64.1329C68.9977 43.845 73.6196 45.9189 76.7856 49.5225ZM46.2975 36.5279H67.2975C69.7828 36.5279 71.7975 34.5623 71.7975 32.1377C71.7975 29.713 69.7828 27.7474 67.2975 27.7474H46.2975C43.8122 27.7474 41.7975 29.713 41.7975 32.1377C41.7975 34.5623 43.8122 36.5279 46.2975 36.5279Z"
                       stroke="#0086FF"
-                      stroke-width="2.42907"
+                      stroke-width="3.5"
                       stroke-linecap="round"
                       stroke-linejoin="round"
                     />
                   </svg>
                 </div>
-                <h3 className="mt-4 plus  fw-bold">Dentistry</h3>
+                <h3 className="mt-4 plus  fw-bold">Pharmacy</h3>
               </div>
-                <h5 className="inter speciality-subheading text-center f-19 fw-normal">Get consultation Form our Dentistry team</h5>
+              <h5 className="inter speciality-subheading text-center f-19 fw-normal">
+                Get consultation form our Pharmacy team
+              </h5>
             </div>
             <div className="col-lg-3 speciality-col">
               <div>
-                <div className="scpeciality-icon-box d-flex align-items-center justify-content-center">
+                <div className="scpeciality-icon-box mx-auto d-flex align-items-center justify-content-center">
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
-                    width="50"
-                    height="50"
-                    viewBox="0 0 50 50"
+                    width="115"
+                    height="114"
+                    viewBox="0 0 115 114"
                     fill="none"
                   >
+                    <rect
+                      x="0.693726"
+                      y="0.301086"
+                      width="113.356"
+                      height="113.356"
+                      rx="25.91"
+                      fill="#F6F9FE"
+                    />
                     <path
-                      d="M26.7683 16.9131L31.0623 12.619M17.4646 17.6287L31.778 31.9421M8.87651 40.5302C4.92397 36.5777 4.92397 30.1693 8.87651 26.2168L26.0526 9.0407C30.0051 5.08816 36.4135 5.08816 40.366 9.0407C44.3185 12.9932 44.3185 19.4016 40.366 23.3541L23.1899 40.5302C19.2374 44.4827 12.8291 44.4827 8.87651 40.5302Z"
+                      d="M33.7975 61.7944H49.8544C52.3247 61.7944 54.795 65.4253 57.2653 65.4253C59.4004 65.4253 63.3809 63.617 65.2193 67.0337C65.8004 68.1136 65.0244 69.3474 63.8735 69.815C61.2079 70.8979 58.2389 72.369 57.2653 72.6871L49.8544 70.2665M65.9113 67.8459L77.2153 63.4153C77.9016 63.1463 78.6479 63.0586 79.3795 63.1611C81.9654 63.523 82.6959 66.842 80.4921 68.2161L61.6095 79.9906C60.4177 80.7337 58.9581 80.9414 57.5998 80.5611L33.7975 73.8973M51.0895 44.8504V50.9018C51.0895 54.2439 53.8545 56.9533 57.2653 56.9533C60.676 56.9533 63.441 54.2439 63.441 50.9018V44.8504M51.0895 44.8504V38.7989C51.0895 35.4568 53.8545 32.7474 57.2653 32.7474C60.676 32.7474 63.441 35.4568 63.441 38.7989V44.8504M51.0895 44.8504H63.441"
                       stroke="#0086FF"
-                      stroke-width="2.42907"
+                      stroke-width="2.5"
                       stroke-linecap="round"
                       stroke-linejoin="round"
                     />
                   </svg>
                 </div>
-                <h3 className="mt-4 plus  fw-bold">Dentistry</h3>
+                <h3 className="mt-4 plus  fw-bold">Trained Staff</h3>
               </div>
-                <h5 className="inter speciality-subheading text-center f-19 fw-normal">Get consultation Form our Dentistry team</h5>
+              <h5 className="inter speciality-subheading text-center f-19 fw-normal">
+                Get consultation form our Trained staff team
+              </h5>
+            </div>
+          </div>
+
+          <div className="row mt-lg-5 justify-content-center gap-5">
+            <div className="col-lg-3 speciality-col">
+              <div>
+                <div className="scpeciality-icon-box mx-auto d-flex align-items-center justify-content-center">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="49"
+                    height="49"
+                    viewBox="0 0 49 49"
+                    fill="none"
+                  >
+                    <path
+                      d="M12.7975 44.4948V36.4948C12.7975 32.9408 14.3425 29.7477 16.7975 27.5504M36.7975 44.4948V36.4948C36.7975 29.8674 31.425 24.4948 24.7975 24.4948C18.1701 24.4948 12.7975 19.1222 12.7975 12.4948V4.49481M36.7975 4.49481V12.4948C36.7975 16.0488 35.2525 19.242 32.7975 21.4392M20.7975 10.4948H28.7975M20.7975 38.4948H28.7975"
+                      stroke="#0086FF"
+                      stroke-width="2.5"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                    />
+                  </svg>
+                </div>
+                <h3 className="mt-4 plus  fw-bold">DNA Mapping</h3>
+              </div>
+              <h5 className="inter speciality-subheading text-center f-19 fw-normal">
+                Get consultation form our DNA Mapping team
+              </h5>
+            </div>
+            <div className="col-lg-3 speciality-col">
+              <div>
+                <div className="scpeciality-icon-box mx-auto d-flex align-items-center justify-content-center">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="114"
+                    height="114"
+                    viewBox="0 0 114 114"
+                    fill="none"
+                  >
+                    <rect
+                      x="0.0639801"
+                      y="0.117676"
+                      width="113.356"
+                      height="113.356"
+                      rx="25.91"
+                      fill="#F6F9FE"
+                    />
+                  </svg>
+                </div>
+                <h3 className="mt-4 plus  fw-bold">Ophthalmology</h3>
+              </div>
+              <h5 className="inter speciality-subheading text-center f-19 fw-normal">
+                Get consultation form our  Ophthalmology team
+              </h5>
+            </div>
+            <div className="col-lg-3 speciality-col">
+              <div>
+                <div className="scpeciality-icon-box mx-auto d-flex align-items-center justify-content-center">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="49"
+                    height="49"
+                    viewBox="0 0 49 49"
+                    fill="none"
+                  >
+                    <path
+                      d="M18.7975 29.4948H24.7975M24.7975 29.4948H30.7975M24.7975 29.4948V23.4948M24.7975 29.4948V35.4948M6.79749 16.4948V34.4948C6.79749 38.9131 10.3792 42.4948 14.7975 42.4948H34.7975C39.2158 42.4948 42.7975 38.9131 42.7975 34.4948V16.4948M6.79749 16.4948H42.7975M6.79749 16.4948C6.79749 12.0765 10.3792 8.49481 14.7975 8.49481M42.7975 16.4948C42.7975 12.0765 39.2158 8.49481 34.7975 8.49481M14.7975 4.49481V8.49481M14.7975 8.49481H34.7975M34.7975 4.49481V8.49481"
+                      stroke="#0086FF"
+                      stroke-width="2.5"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                    />
+                  </svg>
+                </div>
+                <h3 className="mt-4 plus  fw-bold">Medical Aid </h3>
+              </div>
+              <h5 className="inter speciality-subheading text-center f-19 fw-normal">
+                Get consultation form our Trained staff team
+              </h5>
             </div>
           </div>
         </div>
